b07f9f148be619d47b473013c9d8393a21f86d33
Zobair <ahrari1023@gmail.com>
2021-09-07 19:56:17 +0430

    text-widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyContainer(),
+      home: TextWidget(),
     );
   }
 }
@@ -25,34 +25,11 @@ class TextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class MyContainer extends StatelessWidget {
-  const MyContainer({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2d2425),
       body: Center(
-        child: Container(
-          width: 250.0,
-          height: 250.0,
-          decoration: BoxDecoration(
-            color: Color(0xFF2d2425),
-            border: Border.all(color: Color(0xFFd1a497), width: 2),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Center(
-              child: Text(
-            "Container",
-            style: TextStyle(
-                color: Color(0xFF00d0b5),
-                fontSize: 32,
-                fontWeight: FontWeight.bold),
-          )),
+        child: Text(
+          "TEXT",
+          style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
         ),
       ),
     );
