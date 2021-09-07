1e34a92a90cfbd0e26ea5763c09301dd9efaccf3
Zobair <ahrari1023@gmail.com>
2021-09-07 20:17:03 +0430

    container

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: ContainerWidget(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      backgroundColor: Color(0xFF2d2425),
+      body: Center(
+        child: Container(
+          width: 250.0,
+          height: 250.0,
+          decoration: BoxDecoration(
+            color: Color(0xFF2d2425),
+            border: Border.all(color: Color(0xFFd1a497), width: 2),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Center(
+              child: Text(
+            "Container",
+            style: TextStyle(
+                color: Color(0xFF00d0b5),
+                fontSize: 32,
+                fontWeight: FontWeight.bold),
+          )),
+        ),
+      ),
     );
   }
 }
