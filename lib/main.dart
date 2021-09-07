7dcab8683d545bbf37802ad64aae04d2e67ef375
Zobair <ahrari1023@gmail.com>
2021-09-07 20:31:14 +0430

    Flutter Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,15 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Flutter Widgets",
+            style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
