cb0a366a82568883956298d603a2cb247d67e8bb
Zobair <ahrari1023@gmail.com>
2021-09-09 19:57:03 +0430

    row widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: RowWidget(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class RowWidget extends StatelessWidget {
+  const RowWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          child: Text(
-            "Flutter Widgets",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                  color: Color(0xFFf26d7d),
+                  borderRadius: BorderRadius.circular(12)),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Container(
+              width: 80,
+              height: 160,
+              decoration: BoxDecoration(
+                  color: Color(0xFF1cbbb4),
+                  borderRadius: BorderRadius.circular(12)),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                  color: Color(0xFF8560a8),
+                  borderRadius: BorderRadius.circular(12)),
+            ),
+          ],
         ),
       ),
     );
