a7fcbb69e6cc98654e703f13c4ed112afe0d86da
Zobair <ahrari1023@gmail.com>
2021-09-09 17:50:15 +0430

    add textAlign

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
+      body: Center(
+        child: Container(
           child: Text(
             "Flutter Widgets",
+            textAlign: TextAlign.center,
             style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
           ),
         ),
